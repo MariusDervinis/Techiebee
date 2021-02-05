7e044ea69e5a299cac2e466e668afbb0c2706d3c
MariusDervinis <mariux@hotmail.lt>
2021-02-05 16:19:19 +0200

    minor css update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -66,7 +66,7 @@ function App() {
               <i className="fa fa-bars"></i>
             </button>
             <Link className="brand" to="/">
-              amazona
+              Techiebee
             </Link>
           </div>
           <div>
